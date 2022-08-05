1f67abd50dd714415f9b6a155b47e37975060514
Henrik T Sykora <henrik.sykora@gmail.com>
2022-08-05 13:23:53 +0100

    Cleanup

--- src_old/utils.jl
+++
@@ -1,8 +0,0 @@
-function (f::Scalar_Or_Function{rT})(x) where rT<:Number
-    f.f
-end
-function (f::Scalar_Or_Function{rT})(x) where rT<:Function
-    f.f(x)
-end
-
-get_f_type(f::Scalar_Or_Function{rT}) where rT<:Number = rT
